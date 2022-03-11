6b01f16612d3d0b9e8e551abf43bd67303c6b844
de Arruda Vieira Junior Paulo Cesar <paulo-cesar.junior@scania.com>
2022-03-11 15:32:40 +0100

    fix: get error message to display the correct message

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,29 @@
 import artists.ArtistsFile;
-//import artists.ArtistsName;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        create a list of strings to save all the names given in the prompt.
         ArrayList<String> artistsName = new ArrayList<String>();
 
-        ArtistsFile ArtistsFilePath = new ArtistsFile("./artists.txt");
+//        Create a class ArtisitsFile and pass a file to save the names from the list.
+        ArtistsFile ArtistsFilePath = new ArtistsFile("./IO_fundamentals_Java/artists.txt");
 
+//         print all existing names in the file.
         ArtistsFilePath.printArtistsName();
 
+//        using the prompt to get all names to save.
         try(Scanner scanner = new Scanner(System.in)) {
-            String input;
-            do {
+            String input = "";
+            while (!(input.equals("exit"))) {
                 System.out.print("Please enter a musical artist: ");
                 input = scanner.nextLine();
-//                ArtistsName ArtistsName = new ArtistsName(input);
-//                singleAddress.add(input);
-                if (!(input.equals("exit"))) {
-                    artistsName.add(input);
-                }
-
-            }while (!(input.equals("exit")));
+                artistsName.add(input);
+            }
         }
+//        Pass the list of strings to save all the names in the artists.txt file
         ArtistsFilePath.writeArtistsName(artistsName);
-//        System.out.println(singleAddress);
-
 
     }
 }
--- src/artists/ArtistsFile.java
+++ src/artists/ArtistsFile.java
@@ -1,23 +1,18 @@
 package artists;
 
 import java.io.*;
-import java.net.PortUnreachableException;
 import java.util.ArrayList;
 
 public class ArtistsFile {
+    /**Path to the file to be edited*/
     private String artistFilePath;
-//    private ArrayList<String> singleAddress;
-//    public ArtistsFile(String artistFilePath) {
-//        this.artistFilePath = artistFilePath;
-//    }
-//    private String artistsName;
-//    String artistFilePath = "./artists.txt";
 
+    /**Constructor to be assigned on creation, you need to give file path variable as an argument.*/
     public ArtistsFile(String artistFilePath){
         this.artistFilePath = artistFilePath;
     }
 
-
+    /**This function will print the name of all the artists saved in the file*/
     public void printArtistsName() {
         try(FileReader artistsInputFromFile = new FileReader(artistFilePath);
             BufferedReader artistsReaderFromFile = new BufferedReader(artistsInputFromFile)){
@@ -25,22 +20,23 @@ public class ArtistsFile {
             while((line = artistsReaderFromFile.readLine()) != null){
                 System.out.println(line);
             }
+        }catch (FileNotFoundException e){
+            System.out.println("Artist database is currently empty");
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
+
+    /**This function will save all the names given in the artists file*/
     public void writeArtistsName(ArrayList<String> artistsName) {
         try( FileWriter input = new FileWriter(artistFilePath, true);
             BufferedWriter output = new BufferedWriter(input)){
             for (int i = 0; i < artistsName.size(); i++) {
                 output.write(artistsName.get(i) + "\n");
             }
-
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
 }
-//public artist(String artistName){
-//
-//}
+
