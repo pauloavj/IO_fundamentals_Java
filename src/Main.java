0368797d4accac52ba619a7fcd6fd9c12f7cee34
de Arruda Vieira Junior Paulo Cesar <paulo-cesar.junior@scania.com>
2022-03-14 08:03:43 +0100

    fact: extra features

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        create a list of strings to save all the names given in the prompt.
+        // create a list of strings to save all the names given in the prompt.
         ArrayList<String> artistsName = new ArrayList<String>();
 
-//        Create a class ArtisitsFile and pass a file to save the names from the list.
+        // Create a class ArtisitsFile and pass a file to save the names from the list.
         ArtistsFile ArtistsFilePath = new ArtistsFile("./IO_fundamentals_Java/artists.txt");
 
-//         print all existing names in the file.
+        // print all existing names in the file.
         ArtistsFilePath.printArtistsName();
 
-//        using the prompt to get all names to save.
+        // using the prompt to get all names to save.
         try(Scanner scanner = new Scanner(System.in)) {
             String input = "";
             while (!(input.equals("exit"))) {
                 System.out.print("Please enter a musical artist: ");
                 input = scanner.nextLine();
+                // if input equal display show artists names
+                if (input.equals("display")) {
+                    for (int i = 0; i < artistsName.size(); i++) {
+                        System.out.println(artistsName.get(i));
+                    }
+                }
+                // if input equal to clear, clear artists name to be inserted
+                if (input.equals("clear")) {
+                    artistsName.clear();
+                    continue;
+                }
                 artistsName.add(input);
             }
         }
-//        Pass the list of strings to save all the names in the artists.txt file
+        // Pass the list of strings to save all the names in the artists.txt file
         ArtistsFilePath.writeArtistsName(artistsName);
 
     }
--- src/artists/ArtistsFile.java
+++ src/artists/ArtistsFile.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class ArtistsFile {
-    /**Path to the file to be edited*/
+    /**Path to the file to be edited.*/
     private String artistFilePath;
 
     /**Constructor to be assigned on creation, you need to give file path variable as an argument.*/
@@ -12,7 +12,7 @@ public class ArtistsFile {
         this.artistFilePath = artistFilePath;
     }
 
-    /**This function will print the name of all the artists saved in the file*/
+    /**This function will print the name of all the artists saved in the file.*/
     public void printArtistsName() {
         try(FileReader artistsInputFromFile = new FileReader(artistFilePath);
             BufferedReader artistsReaderFromFile = new BufferedReader(artistsInputFromFile)){
@@ -27,7 +27,7 @@ public class ArtistsFile {
         }
     }
 
-    /**This function will save all the names given in the artists file*/
+    /**This function will save all the names given in the artists file.*/
     public void writeArtistsName(ArrayList<String> artistsName) {
         try( FileWriter input = new FileWriter(artistFilePath, true);
             BufferedWriter output = new BufferedWriter(input)){
